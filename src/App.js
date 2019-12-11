bb651753b35dbfa9d3828a17148661aed689cacc
milos1980 <mixy80@gmail.com>
2019-12-11 22:09:01 +0100

    changed app text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hi bre Milose
+          I was changed on feature
         </a>
       </header>
     </div>
